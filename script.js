8ffa793d27f2628799197b1456ccfda7c0f51bd2
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-22 13:06:31 -0500

    Initial

---
+++ script.js
@@ -0,0 +1,16 @@
+var bill = ""
+var qualityService = ""
+var partySize = ""
+
+
+switch (qualityService) {
+    case Great:
+    Tip = bill * 0.2
+        break;
+    case Good:
+    Tip = bill * 0.15
+        break;
+    case Poor:
+    Tip = bill * 0.1
+        break;
+}
