31a34ef4d501bde5c603ce37c9183fa6c4881ae5
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-23 14:55:19 -0500

    Almost Done

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
+
 function calculateAndDisplay(){
     
     let bill = parseFloat(document.getElementById('totalBill').value)
+    let partySize = parseInt(document.getElementById('partySize').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calculateAmtPerPers(totalBill, partySize)
+
+
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBill').innerText = 'Total: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPers').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
 
 }
+
+
