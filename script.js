54d4fdb5bfa656bc79be368f7b385fc3abf35ea4
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-26 13:57:41 -0500

    Done

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
-
+//Function so Occurs on Click
 function calculateAndDisplay(){
-    
+
+    //Getting Values from HTML
     let bill = parseFloat(document.getElementById('totalBill').value)
     let partySize = parseInt(document.getElementById('partySize').value)
     let serviceQuality = document.getElementById('serviceQuality').value
 
 
-    let tip = calculateTip(bill, serviceQuality)
-    let totalBill = calculateTotalBill(bill, tip)
-    let amtPerPers = calculateAmtPerPers(totalBill, partySize)
+ // Calculate Tip, Total, and Amount Per Person
 
+    let tip = calculateTip(bill, serviceQuality);
+    let totalBill = calculateTotalBill(bill, tip);
+    let amtPerPers = calculateAmtPerPers(totalBill, partySize);
 
+    //Display Results
     document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
-    document.getElementById('totalBill').innerText = 'Total: $' + totalBill.toFixed(2)
-    document.getElementById('amtPerPers').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
-
+    document.getElementById('totalBillResult').innerText = 'Total: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersResult').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
+    
 }
 
-
+//Helper Functions
+function calculateTip(bill, serviceQuality){
+    
+    if (serviceQuality === 'great'){
+        return bill * .2
+    } else if (serviceQuality === 'good'){
+        return bill * .15
+    } else if (serviceQuality === 'poor'){
+        return bill * .1
+    }
+    
+}
+function calculateTotalBill(bill, tip){
+    return bill + tip
+}
+function calculateAmtPerPers(totalBill, partySize){
+    return totalBill / partySize
+}
